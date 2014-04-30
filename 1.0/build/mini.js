df0d576b8d115ec694deb7ffb0a94f89515573b2
霸先 <yanqing.li@alibaba-inc.com>
2014-04-30 18:13:25 +0800

    add bind

--- 1.0/build/mini.js
+++ 1.0/build/mini.js
@@ -10,10 +10,18 @@ gallery/order/1.0/mini
  * @author 霸先<baxian@taobao.com>
  * @module order
  **/
+//25 60 67 80 90 100 130 160 180 198 215
 KISSY.add('gallery/order/1.0/index',function (S, Node,Base) {
     var ASC = 'ks-order-ascending';
     var DESC = 'ks-order-descending';
+    var ASCORDER = 'asc';
+    var DESCORDER = 'desc';
+    var ACTIVATEDASC = 'ks-order-activated-ascending';
+    var ACTIVATEDDESC = 'ks-order-activated-descending';
     var ACTIVATED = 'ks-order-activated';
+    var TRIGGER = 'ks-order-trigger';
+    var DOT = '.';
+    var SPACE = ' ';
     /**
      * 
      * @class Order
@@ -43,19 +51,28 @@ KISSY.add('gallery/order/1.0/index',function (S, Node,Base) {
             _.items = [];
             _.wrapper = S.one(_.get('tpl'));
             S.each(data,function(item,index){
-                var cellStr = S.substitute(_.get('itemCellTpl'),item);
-                var itemStr = S.substitute(_.get('itemTpl'),{
-                    index : index,
-                    cell : cellStr
+                if(typeof item.isTwoWay === 'undefined'){
+                    item.isTwoWay = true;
+                }
+                var cellStr = S.substitute(item.itemCellTpl || _.get('itemCellTpl'),item);
+                var itemStr = S.substitute(item.itemTpl || _.get('itemTpl'),{
+                    index : index
                 });
+                var cellNode = S.one(cellStr);
                 var itemNode = S.one(itemStr);
+                itemNode.append(cellNode);
                 if(item.activated){
                     itemNode.addClass(ACTIVATED);
                     _.curNode = itemNode;
                     _.curIndex = index;
                 }
-                if(typeof item.isTwoWay == 'undefined'){
-                    itemNode.addClass(item.default === 'asc' ? ASC : DESC);
+                if(item.isTwoWay){
+                    cellNode.append('<i class="icon">&nbsp;</i>');
+                    if(item.activated){
+                        itemNode.addClass(item.default === 'asc' ? ACTIVATEDASC : ACTIVATEDDESC);
+                    }else{
+                        itemNode.addClass(item.default === 'asc' ? ASC : DESC);
+                    }
                 }
                 _.items.push(itemNode);
                 _.wrapper.append(itemNode);
@@ -63,7 +80,62 @@ KISSY.add('gallery/order/1.0/index',function (S, Node,Base) {
             _.get('node').append(_.wrapper);
         },
         bind : function(){
+            var _ = this;
+            var data = _.get('data');
+            _.wrapper.delegate('click',DOT+TRIGGER,function(e){
+                e.preventDefault();
+                var tar = S.one(e.currentTarget),
+                    parent = tar.parent('li'),
+                    index = parent.attr('data-index'),
+                    oldOrder = 'none',
+                    newOrder = 'none';
+                if(parent.hasClass(ACTIVATED)){
+                    if(data[index].isTwoWay){
+                        if(parent.hasClass(ACTIVATEDASC)){
+                            oldOrder = ASCORDER;
+                            newOrder = DESCORDER;
+                            parent.replaceClass(ACTIVATEDASC,ACTIVATEDDESC);
+                        }else{
+                            oldOrder = DESCORDER;
+                            newOrder = ASCORDER;
+                            parent.replaceClass(ACTIVATEDDESC,ACTIVATEDASC);
+                        }
+                    }
+                }else{
+                    _.curNode.removeClass(ACTIVATED + SPACE + ACTIVATEDDESC + SPACE + ACTIVATEDASC);
+                    _.curNode.addClass(data[_.curIndex].default === ASCORDER ? ASC : DESC);
 
+                    if(parent.hasClass(ASC)){
+                        oldOrder = ASCORDER;
+                        newOrder = DESCORDER;
+                        parent.replaceClass(ASC,ACTIVATEDASC);
+                    }else if(parent.hasClass(DESC)){
+                        oldOrder = DESCORDER;
+                        newOrder = ASCORDER;
+                        parent.replaceClass(DESC,ACTIVATEDDESC);
+                    }
+                    parent.addClass(ACTIVATED);
+                }
+                _.fire('orderChange',{
+                    oldEl : _.curNode,
+                    oldData : data[_.curIndex],
+                    newEl : parent,
+                    newData : data[index],
+                    oldOrder : oldOrder,
+                    newOrder : newOrder
+                });
+                _.curNode = parent;
+                _.curIndex = index;
+            });
+        },
+        getCurrentNode : function(){
+            return this.curNode;
+        },
+        getCurrentData : function(){
+            return this.get('data')[this.curIndex];
+        },
+        getCurrentOrder : function(){
+            return this.curOrder;
         }
     }, {ATTRS : /** @lends Order*/{
         node : {
@@ -92,11 +164,11 @@ KISSY.add('gallery/order/1.0/index',function (S, Node,Base) {
             readonly : true
         },
         itemTpl : {
-            value : '<li class="ks-order-item" data-index="{index}">{cell}</li>',
+            value : '<li class="ks-order-item" data-index="{index}"></li>',
             readonly : true
         },
         itemCellTpl : {
-            value : '<a href="javascript:;">{displayName}</a>'
+            value : '<a href="javascript:;" class="ks-order-trigger">{displayName}</a>'
         }
     }});
     return Order;
